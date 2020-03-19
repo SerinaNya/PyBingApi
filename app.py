471dad5de04921e9d1cb004fc7bd1beeb7101ec5
jinzhijie <742905606@qq.com>
2020-03-19 12:57:18 +0800

    可实现随机图和自定义daysago

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
 from flask import Flask, redirect
 import requests
+import random
 app = Flask(__name__)
+
+BASEDOMAIN = 'https://global.bing.com'
 
 
 @app.route('/bing')
 def bing():
-    BASEDOMAIN = 'https://global.bing.com'
-    req = requests.get(BASEDOMAIN+'/HPImageArchive.aspx?format=js&n=1')
+    req = requests.get(BASEDOMAIN+'/HPImageArchive.aspx?format=js&n=-1')  # 默认第二天/今天
     pic_url = req.json()['images'][0]['url']
-    print(pic_url)
+    return redirect(BASEDOMAIN+pic_url)
+
+
+@app.route('/bing/<daysago>')
+def bing_daysago(daysago):
+    req = requests.get(BASEDOMAIN+'/HPImageArchive.aspx?format=js&idx=%d&n=1' % int(daysago))
+    pic_url = req.json()['images'][0]['url']
+    return redirect(BASEDOMAIN+pic_url)
+
+@app.route('/bing/random')
+def bing_random():
+    req = requests.get(BASEDOMAIN+'/HPImageArchive.aspx?format=js&idx=%d&n=1' % random.randint(-1,7))  # -1 ~ 7天
+    pic_url = req.json()['images'][0]['url']
     return redirect(BASEDOMAIN+pic_url)
 
 
 if __name__ == '__main__':
     app.debug = True
-    app.run('127.0.0.1', 80)
+    app.run('127.0.0.1', 81)
